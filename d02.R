38b7e6d176257deaf5b43675205a79f8ed853674
neekro <smira.martin@gmail.com>
2016-12-10 10:24:14 +0100

    add Day 2 Part 2 solution

--- d02.R
+++ d02.R
@@ -1,9 +1,13 @@
 # Day 2
 
-now = 5
+
 
 input = scan("input02.txt", character())
 nDig = length(input)
+
+now = 5
+
+# Part 1
 
 output = c()
 
@@ -29,5 +33,46 @@ for (i in 1:nDig) {
   output[i] <- now
 }
 
-# Correct answer: 65556
+# Part 1 answer: 65556
+output
+
+
+
+
+# Part 2
+
+output = c()
+
+for (i in 1:nDig) {
+  
+  sequence = input[i]
+  seqLength = nchar(sequence)
+  
+  for (j in 1:seqLength) {
+    
+    instruct <- substr(sequence, j, j)
+    
+    if (instruct == "L" & !(now %in% c(1, 2, 5, 10, 13))) {
+      now = now - 1 
+    } else if (instruct == "R" & !(now %in% c(1, 4, 9, 12, 13))) {
+      now = now + 1 
+    } else if (instruct == "U" & !(now %in% c(1, 2, 4, 5, 9))) {
+      if (now %in% c(3, 13)) {
+        now = now - 2
+      } else {
+        now = now - 4
+      }
+      
+    } else if (instruct == "D" & !(now %in% c(5, 9, 10, 12, 13))) {
+      if (now %in% c(1, 11)) {
+        now = now + 2
+      } else {
+        now = now + 4
+      }
+    }
+  }
+  output[i] <- now
+}
+
+# Part 2 answer: CB779
 output
