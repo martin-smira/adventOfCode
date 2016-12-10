93da99cd7f143ed4269dfca14e007077ff1db407
neekro <smira.martin@gmail.com>
2016-12-10 10:11:53 +0100

    Day 1 fix

--- d01.R
+++ d01.R
@@ -1,18 +1,18 @@
 
 input <- scan("input01.txt", character())
 
+# input <- c("R8", "R4", "R4", "R8")
+
 dir <- substr(input, 1, 1)
 
 len <- as.numeric(gsub(",", "", substr(input, 2, 10)))
-
-
-
 
 
 coordX = c(0)
 coordY = c(0)
 direction = 0
 
+step <- 0
 
 for (i in 1:length(dir)) {
   
@@ -22,30 +22,39 @@ for (i in 1:length(dir)) {
     direction = (direction + 90) %% 360
   }
   
-  if (direction == 0) {
-    coordX[i+1] <- coordX[i]
-    coordY[i+1] <- coordY[i] + len[i]
-  } else if (direction == 90) {
-    coordX[i+1] <- coordX[i] + len[i]
-    coordY[i+1] <- coordY[i]
-  } else if (direction == 180) {
-    coordX[i+1] <- coordX[i]
-    coordY[i+1] <- coordY[i] - len[i]
-  } else if (direction == 270) {
-    coordX[i+1] <- coordX[i] - len[i]
-    coordY[i+1] <- coordY[i]
-  }
-  
-  xMatch = (coordX[i+1] == coordX[-(i+1)])
-  match = coordY[i+1] == coordY[c(xMatch, FALSE)]
-  
-  if (any(match)) {
-    cat(coordX[i+1], coordY[i+1])
+  for (l in 1:len[i]) {
+    step <- step + 1
     
-    break
+    if (direction == 0) {
+      coordX[step + 1] <- coordX[step]
+      coordY[step + 1] <- coordY[step] + 1
+    } else if (direction == 90) {
+      coordX[step + 1] <- coordX[step] + 1
+      coordY[step + 1] <- coordY[step]
+    } else if (direction == 180) {
+      coordX[step + 1] <- coordX[step]
+      coordY[step + 1] <- coordY[step] - 1
+    } else if (direction == 270) {
+      coordX[step + 1] <- coordX[step] - 1
+      coordY[step + 1] <- coordY[step]
+    }
+    # 
+    # xMatch = (coordX[step + 1] == coordX[-(step + 1)])
+    # match = coordY[step + 1] == coordY[c(xMatch, FALSE)]
   }
+  
+  # if (any(match)) {
+  #   cat(coordX[i + 1], coordY[i + 1])
+  #   
+  #   break
+  # }
 }
 
 coor <- cbind(coordX, coordY)
+coor
 
-duplicated(rbind(coor, c(0,0)))
+dup <- which(duplicated(rbind(coor)))
+
+# Part 2 result (=143)
+sum(abs(coor[dup[1], ]))
+
